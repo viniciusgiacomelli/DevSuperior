5ddd99d4f860badfbac72f4aec5b126de39c92be
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2020-09-28 08:52:55 -0300

    publicando...

--- front-mobile/src/pages/CreateRecord/index.tsx
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -13,7 +13,7 @@ const placeholder = {
     value: null
 }
 
-const BASE_URL = 'http://10.0.0.200:8080';
+const BASE_URL = 'https://sds1-v.herokuapp.com';
 
 const mapSelectValue = (games: Game[]) => {
     return games.map(game => ({
