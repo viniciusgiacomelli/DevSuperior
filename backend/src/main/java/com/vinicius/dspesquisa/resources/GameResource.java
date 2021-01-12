13a8c89ffbe8c82ae28c9fada7cc6ca349a57675
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-01-12 19:23:31 -0300

    @CrossOrigin no endpoint

--- backend/src/main/java/com/vinicius/dspesquisa/resources/GameResource.java
+++ backend/src/main/java/com/vinicius/dspesquisa/resources/GameResource.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinicius.dspesquisa.dto.GameDTO;
-import com.vinicius.dspesquisa.entity.Game;
-import com.vinicius.dspesquisa.repositories.GameRepository;
 import com.vinicius.dspesquisa.services.GameService;
 
 @RestController
@@ -20,6 +19,7 @@ public class GameResource {
 	@Autowired
 	private GameService service;
 	
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<GameDTO>> findAll(){
 		List<GameDTO> list = service.findAll();
--- backend/src/main/java/com/vinicius/dspesquisa/resources/RecordResource.java
+++ backend/src/main/java/com/vinicius/dspesquisa/resources/RecordResource.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,13 +26,14 @@ public class RecordResource {
 	@Autowired
 	private RecordService service;
 	
+	@CrossOrigin
 	@PostMapping
 	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto){
 		RecordDTO newDTO = service.insert(dto);
 		return ResponseEntity.ok().body(newDTO);
 	}
 
-
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<Page<RecordDTO>> findAll(
 			@RequestParam(value = "min", defaultValue = "")String min,
